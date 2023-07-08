4126104796ea1434009ccb01f3be6025e227f5a3
LuisMtz1899692 <luisalonso20101@hotmail.com>
2023-07-08 03:33:11 -0600

    Nuevos Cambios Server

---
+++ assets/js/serveronline2.js
@@ -0,0 +1,28 @@
+function checkServerStatus() {
+    var serverIP = '185.107.192.114:37975'; // Reemplaza con la IP de tu servidor Minecraft
+  
+    fetch('https://api.mcsrvstat.us/bedrock/2/' + serverIP)
+      .then(response => response.json())
+      .then(data => {
+        var serverStatusElement = document.getElementById('server-status');
+        var serverImageElement = document.getElementById('server-image');
+  
+        if (data.online) {
+          serverStatusElement.textContent = 'El servidor está en línea.';
+          serverImageElement.src = 'assets/img/online.png'; // Ruta a la imagen para el estado en línea
+          serverImageElement.alt = 'Servidor en línea';
+        } else {
+          serverStatusElement.textContent = 'El servidor está fuera de línea.';
+          serverImageElement.src = 'assets/img/ofline.png'; // Ruta a la imagen para el estado fuera de línea
+          serverImageElement.alt = 'Servidor fuera de línea';
+        }
+      })
+      .catch(error => {
+        console.error('Error al obtener el estado del servidor:', error);
+        var serverStatusElement = document.getElementById('server-status');
+        serverStatusElement.textContent = 'Error al obtener el estado del servidor.';
+      });
+  }
+  
+  // Actualiza el estado del servidor cada 5 segundos
+  setInterval(checkServerStatus, 5000);
