fd3fb71dabcab620073692f4e6b814881b7c5611
LuisMtz1899692 <luisalonso20101@hotmail.com>
2023-07-12 18:09:10 -0600

    Correccion final ip

--- assets/js/serveronline.js
+++ assets/js/serveronline.js
@@ -6,21 +6,30 @@ function checkServerStatus() {
       .then(data => {
         var serverStatusElement = document.getElementById('server-status2');
         var serverImageElement = document.getElementById('server-image2');
+        var ServerImageseñalElement = document.getElementById('server-onlineimg2');
   
         if (data.online) {
           serverStatusElement.textContent = 'El servidor está en línea.';
           serverImageElement.src = 'assets/img/online.png'; // Ruta a la imagen para el estado en línea
           serverImageElement.alt = 'Servidor en línea';
+          ServerImageseñalElement.src = 'assets/img/senal.png'
+          ServerImageseñalElement.alt = 'Contador jugadores'
+          document.getElementById('player-count2').textContent = 'Jugadores en línea: ' + data.players.online  + '/' + data.players.max;
         } else {
           serverStatusElement.textContent = 'El servidor está fuera de línea.';
           serverImageElement.src = 'assets/img/ofline.png'; // Ruta a la imagen para el estado fuera de línea
           serverImageElement.alt = 'Servidor fuera de línea';
+          ServerImageseñalElement.src = 'assets/img/señal2.png';
+          ServerImageseñalElement.alt = 'Contador jugadores'
+          document.getElementById('player-count2').textContent = '';
         }
       })
       .catch(error => {
         console.error('Error al obtener el estado del servidor:', error);
         var serverStatusElement = document.getElementById('server-status');
         serverStatusElement.textContent = 'Error al obtener el estado del servidor.';
+        ServerImageseñalElement.src = 'assets/img/señal2.png';
+        document.getElementById('player-count').textContent = '';
       });
   }
   
